0bdea3f7e9f981cd5f59ac6346ecfe41e104b615
Viraj Bhalala <virajbhalala@gmail.com>
2015-03-22 18:18:25 -0400

    update

--- src/p2_15s_vb97.java
+++ src/p2_15s_vb97.java
@@ -1,7 +1,24 @@
+import java.util.*;
+
 
 public class p2_15s_vb97 {
 	public static void main(String[] args){
 		
+		System.out.println("Do you want to enter a string?");
+		Scanner s= new Scanner(System.in);
+		String answer = s.nextLine();
+		
+		
+		
+		
+	}
+	Stack stack=new Stack();
+
+	public static String printState(){
+		return "";
+		
 	}
 
 }
+
+	
