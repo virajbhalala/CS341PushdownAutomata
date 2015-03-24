7ae0164198fa3c609b5a7ff22fbf88bd95d0c2d3
Viraj Bhalala <virajbhalala@gmail.com>
2015-03-23 21:43:47 -0400

    update

--- src/p2_15s_vb97.java
+++ src/p2_15s_vb97.java
@@ -32,23 +32,23 @@ public class p2_15s_vb97 {
 
 	//read,pop->push
 	public static void start(String input){
-		int index=0;
 		//System.out.println("assume £ is an epsilon symbol");
 		if (input.charAt(0)=='$'){
 			stack.push("$");
 			printOut("$","epsilon","$","q1", "q2");
 			index++;
+			
 			if(Character.isLetter(input.charAt(index))){
 				printOut(input.substring(index, index+1),"epsilon","epsilon","q2", "q3");
 				//stack.push(input.substring(index, index+1));
-				q3(input);
 				index++;
+				q3(input);
 			}
 			else if(input.charAt(index) == '('){
 				stack.push("(");
 				printOut(input.substring(index, index+1),"epsilon","(","q1", "q5");
-				q5(input);
 				index++;
+				q5(input);
 				}
 			else
 			{
@@ -68,17 +68,24 @@ public class p2_15s_vb97 {
 	public static void q3(String input){
 		if (Character.isLetter(input.charAt(index)) || Character.isDigit(input.charAt(index)) || input.charAt(index) == '_' ){
 			printOut(input.substring(index, index+1),"epsilon","epsilon","q3", "q3");
-			q3(input);
 			index++;
+			q3(input);
 		}
 		else if(input.substring(index, index+1).contains(operators)){
 			printOut(input.substring(index, index+1),"epsilon","epsilon","q3", "q4");
-			q4(input);
 			index++;
+			q4(input);
 			
 		}
 	
 		//else reject();
+		else if (input.charAt(index) == ')'){
+			stack.push("(");
+			printOut(input.substring(index, index+1),"(","epsilon","q3", "q6");
+			index++;
+			q6(input);
+		}
+		else reject();
 		
 		
 	}
@@ -86,14 +93,14 @@ public class p2_15s_vb97 {
 		if(input.charAt(index) == '('){
 			stack.push("(");
 			printOut(input.substring(index, index+1),"epsilon","(","q4", "q5");
-			q5(input);
 			index++;
+			q5(input);
 		}
 		else if(Character.isLetter(input.charAt(index))){
 			printOut(input.substring(index, index+1),"epsilon","epsilon","q4", "q5");
 			//stack.push(input.substring(index, index+1));
-			q3(input);
 			index++;
+			q3(input);
 		}
 		else reject();
 		
@@ -103,22 +110,44 @@ public class p2_15s_vb97 {
 		if(input.charAt(index) == '('){
 			stack.push("(");
 			printOut(input.substring(index, index+1),"epsilon","(","q5", "q5");
-			q5(input);
 			index++;
+			q5(input);
+			
 		}
 		else if(Character.isLetter(input.charAt(index))){
 			printOut(input.substring(index, index+1),"epsilon","epsilon","q5", "q3");
 			//stack.push(input.substring(index, index+1));
-			q3(input);
 			index++;
+			q3(input);
 		}
 		else reject();
 	
 	}
 	public static void q6(String input){
+		if (input.charAt(index) == ')'){
+			stack.push("(");
+			printOut(input.substring(index, index+1),"(","epsilon","q3", "q6");
+			index++;
+			q6(input);
+		}
+		else if(input.substring(index, index+1).contains(operators)){
+			printOut(input.substring(index, index+1),"epsilon","epsilon","q3", "q4");
+			index++;
+			q4(input);
+			
+		} 
+		else if (input.charAt(0)=='$'){
+			stack.push("$");
+			printOut("$","epsilon","$","q1", "q2");
+			index++;
+			q7(input);
+		}
+		else
+			reject();
 	
 	}
 	public static void q7(String input){
+		
 	
 	}
 		
